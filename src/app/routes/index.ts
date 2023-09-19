eb5fbb3434ae0cfc0567336511fcdba12f1f6aed
md kamruzzaman <kamruzzamanmd@hotmail.com>
2023-09-19 10:11:37 +0600

    [update]: offeredcousre and semester registion module

--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -5,7 +5,11 @@ import { AcademicSemeterRoutes } from '../modules/academicSemester/academicSemes
 import { buildingRoutes } from '../modules/building/building.routes';
 import { courseRoutes } from '../modules/course/course.routes';
 import { facultyRoutes } from '../modules/faculty/faculty.routes';
+import { offeredCourseRoutes } from '../modules/offeredCourse/offeredCourse.routes';
+import { offeredCourseClassScheduleRoutes } from '../modules/offeredCourseClassSchedule/offeredCourseClassSchedule.routes';
+import { offeredCourseSectionRoutes } from '../modules/offeredCourseSection/offeredCourseSection.routes';
 import { roomRoutes } from '../modules/room/room.routes';
+import { semesterRegistrationRoutes } from '../modules/semesterRegistration/semesterRegistration.routes';
 import { studentRoutes } from '../modules/student/student.routes';
 
 const router = express.Router();
@@ -20,6 +24,13 @@ const moduleRoutes = [
   { path: '/buildings', route: buildingRoutes },
   { path: '/rooms', route: roomRoutes },
   { path: '/courses', route: courseRoutes },
+  { path: '/semester-registration', route: semesterRegistrationRoutes },
+  { path: '/offered-courses', route: offeredCourseRoutes },
+  { path: '/offered-course-sections', route: offeredCourseSectionRoutes },
+  {
+    path: '/offered-course-class-schedules',
+    route: offeredCourseClassScheduleRoutes,
+  },
 ];
 
 moduleRoutes.forEach(route => router.use(route.path, route.route));
